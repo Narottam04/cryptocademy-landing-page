3ad6547747fd9ebeea9c3e7195d3eb653bd66e57
Narottam04 <coutkunal@gmail.com>
2022-04-24 17:07:24 +0530

    added meta tags to head

--- pages/contact.js
+++ pages/contact.js
@@ -4,12 +4,26 @@ import { Footer } from '../components/Footer'
 const contact = () => {
   return (
     <>
+        <Head>
+        <title>Cryptocademy - Learn to invest like a pro.</title>
+        <link rel="icon" href="/icon.png" />
+        <meta name="description" content="Cryptocademy is an app that teaches people how to trade cryptocurrencies and invest in coins like bitcoin,ethereum etc. User can analyze market data by viewing charts, top daily cryptocurrency news. Through our curated blogs and courses,users can also learn more about cryptocurrency and blockchain concepts."/>
+        <link rel="canonical" href="https://cryptocademy.netlify.app/"/>
+        <meta property="og:title" content="Learn to invest like a pro"/>
+        <meta property="og:description" content="Cryptocademy is an app that teaches people how to trade cryptocurrencies and invest in coins like bitcoin,ethereum etc. User can analyze market data by viewing charts, top daily cryptocurrency news. Through our curated blogs and courses,users can also learn more about cryptocurrency and blockchain concepts."/>
+        <meta property="og:image" content="/icon.png"/>
+        <meta property="og:image:width" content="2727"/>
+        <meta property="og:image:height" content="1952"/>
+        <meta property="og:site_name" content="Cryptocademy"/>
+        <meta property="og:type" content="Trading,Investment,Courses,Learning"/>
+        <meta name="language" content="EN"/>
+        <meta name="author" content="Cryptocademy"/>
+      </Head>
         <Nav/>
         <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
             <h2 className="mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl">
                 <span className="block w-full text-3xl md:text-4xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500 lg:inline">Submit a request</span>
             </h2>
-            {/* https://formsubmit.co/el/wilizi */}
             <form action="https://formsubmit.co/webdripdev@gmail.com" method="POST">
                 
                 <div class="relative z-0 mb-6 max-w-2xl group">
--- pages/faq.js
+++ pages/faq.js
@@ -3,6 +3,21 @@ import { Footer } from '../components/Footer'
 const faq = () => {
     return (
     <section>
+      <Head>
+        <title>Cryptocademy - Learn to invest like a pro.</title>
+        <link rel="icon" href="/icon.png" />
+        <meta name="description" content="Cryptocademy is an app that teaches people how to trade cryptocurrencies and invest in coins like bitcoin,ethereum etc. User can analyze market data by viewing charts, top daily cryptocurrency news. Through our curated blogs and courses,users can also learn more about cryptocurrency and blockchain concepts."/>
+        <link rel="canonical" href="https://cryptocademy.netlify.app/"/>
+        <meta property="og:title" content="Learn to invest like a pro"/>
+        <meta property="og:description" content="Cryptocademy is an app that teaches people how to trade cryptocurrencies and invest in coins like bitcoin,ethereum etc. User can analyze market data by viewing charts, top daily cryptocurrency news. Through our curated blogs and courses,users can also learn more about cryptocurrency and blockchain concepts."/>
+        <meta property="og:image" content="/icon.png"/>
+        <meta property="og:image:width" content="2727"/>
+        <meta property="og:image:height" content="1952"/>
+        <meta property="og:site_name" content="Cryptocademy"/>
+        <meta property="og:type" content="Trading,Investment,Courses,Learning"/>
+        <meta name="language" content="EN"/>
+        <meta name="author" content="Cryptocademy"/>
+      </Head>
       <Nav/>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
         <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
